53c056792ac95ad041ca06899e200ad6a9fd56a9
Imane-imz <imanemardhy@gmail.com>
2025-01-15 17:21:36 +0100

    add homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $productRepository->findBy([], ['id'=>'DESC']),
+            'categories' => $categoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('/product/{id}/show', name: 'app_product_page_show', methods: ['GET'])]
+    public function showProduct(Product $product, ProductRepository $productRepository): Response
+    {
+        $lastProducts = $productRepository->findBy([], ['id' => 'DESC'], 4);
+
+        return $this->render('home/show.html.twig', [
+            'product' => $product,
+            'products' => $lastProducts
         ]);
     }
 }
