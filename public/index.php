bc3be575417d925cf9521546603d04a4a2cb0dfc
Imane-imz <imanemardhy@gmail.com>
2025-04-19 14:25:48 +0200

    update index.php

--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', (bool) ($_SERVER['APP_DEBUG'] ?? true));
-$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
